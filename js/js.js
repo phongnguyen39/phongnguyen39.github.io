f91d3f83da950bba7362fd787673e4594fd6c7a9
phong_nguyen <phong.nguyen39@gmail.com>
2019-06-28 22:24:05 -0700

    started fixing GitHub API throttling

--- js/js.js
+++ js/js.js
@@ -3,9 +3,12 @@ var request = new XMLHttpRequest();
 request.open('get', 'https://api.github.com/users/phongnguyen39', true);
 request.onload = function () {
     var data = JSON.parse(this.response);
-    // console.log(data);
-    document.getElementById("funStat").innerHTML = data.public_repos + " repos";
-    /////  come back to this and show it on the page
+    if (data.message == "API rate limit exceeded for 73.71.99.20. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)") {
+        console.log("API Calls to GitHub exceeded")
+    }
+    else {
+        document.getElementById("funStat").innerHTML = " | " + data.public_repos + " repos";
+    }
 }
 request.send();
 
@@ -14,41 +17,41 @@ request.send();
 var ratingsUp = 0;
 var ratingsDown = 0;
 
-function ratingUp(){
-    ratingsUp +=1;
+function ratingUp() {
+    ratingsUp += 1;
     console.log(ratingsUp)
 }
 
-function ratingDown(){
-    ratingsDown +=1;
+function ratingDown() {
+    ratingsDown += 1;
     console.log(ratingsDown);
 }
 
 //Indexdb testing /////////////////
 
-let openRequest = indexedDB.open("Ratings",1);
+// let openRequest = indexedDB.open("Ratings",1);
 
-openRequest.onupgradeneeded = function(){
-    console.log("User browser does not support IndexedDB")
-}
+// openRequest.onupgradeneeded = function(){
+//     console.log("User browser does not support IndexedDB")
+// }
 
-openRequest.onerror = function(){
-    console.error("Error",openRequest.error);
-}
+// openRequest.onerror = function(){
+//     console.error("Error",openRequest.error);
+// }
 
-openRequest.onsuccess = function(){
-    let db = openRequest.result;
-    // things I want to store... datetime, yes or no
+// openRequest.onsuccess = function(){
+//     let db = openRequest.result;
+//     // things I want to store... datetime, yes or no
 
-    db.createObjectStore("datetime",{keyPath:"id"})
-    console.log("db online");
-    console.log(db)
-}
+//     db.createObjectStore("datetime",{keyPath:"id"})
+//     console.log("db online");
+//     console.log(db)
+// }
 
-function ratingUp(){
+function ratingUp() {
     console.log("Rating Up")
 }
 
-function ratingDown(){
+function ratingDown() {
     console.log("Rating Down")
 }
